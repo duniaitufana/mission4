9f2e692b85abd40ac041b55f05fdbc256ceea234
duniaitufana <nauvaljaiz.hk@gmail.com>
2024-10-28 19:22:33 +0300

    fix bug

--- JS/main-home.js
+++ JS/main-home.js
@@ -37,6 +37,7 @@ const getSVGInSwitchEl = document.querySelectorAll('.switch-list svg,.switch-pro
 const switchEl = document.querySelector('.switch');
 const mainContentDiv = document.querySelector('.main-content > div');
 const widthContentTask = document.querySelector('.main-content > div section');
+const listCalender_ = document.querySelector('.list-calender');
 
 textArea.value = "";
 getSVGInSwitchEl[0].style.borderBottom = ".6rem solid #387478";
@@ -556,6 +557,21 @@ function switchValidation() {
     })
 }
 
+function warnResizeCalender(){
+    listCalender_.innerHTML = `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 512 512" width="30" height="30" xml:space="preserve">
+<polygon style="fill:#FFA418;" points="0,477.703 256,477.703 289.391,256 256,34.297 "/>
+<polygon style="fill:#FF8A1E;" points="256,34.297 256,477.703 512,477.703 "/>
+<g>
+	<circle style="fill:#324860;" cx="256" cy="405.359" r="16.696"/>
+	<rect x="239.304" y="177.185" style="fill:#324860;" width="33.391" height="178.087"/>
+</g>
+</svg><p>Anda melakukan resize di browser, silahkan refresh untuk mendapatkan tanggal di bagian ini</p>`;
+    listCalender_.style.textAlign = "center";
+    listCalender_.style.fontSize = "1.7rem";
+    listCalender_.style.padding = "3rem 2rem";
+}
+
 btnAddTask.addEventListener("click",()=> {
     if (sectionManageTask.dataset.hide === "close") {
         sectionManageTask.dataset.hide = "open";
@@ -648,6 +664,7 @@ window.addEventListener("resize",()=>{
     mainContentDiv.style.transform = "translateX(0px)"
     switchValidation();
     getSVGInSwitchEl[0].style.borderBottom = ".6rem solid #387478";
+    warnResizeCalender();
 })
 
 timer();
